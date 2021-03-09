af23a9f96bb1c9f88408644e73e4ccc26a30be55
ELVISION <maknae12@gmail.com>
2021-03-09 10:00:28 +0900

    테스트중

--- app/src/main/java/org/androidtown/launchintent/features/MainActivity.kt
+++ app/src/main/java/org/androidtown/launchintent/features/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		initialize()
-
 	//val manager = getSystemService(Context.USB_SERVICE) as UsbManager
 	//val deviceList : HashMap<String, UsbDevice> = manager.deviceList
 	//var device = deviceList["deviceName"]
